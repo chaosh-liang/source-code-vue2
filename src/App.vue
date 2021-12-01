28ff33c1db7eda6c156be9e87bcef3ed73c69a24
chaosh-liang <chao.liang.fly@gmail.com>
2021-12-01 18:50:46 +0800

    feat: save

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" @user-click="userClick">
+      <!-- v-slot 指令自 Vue 2.6.0 起被引入，提供更好的支持 slot 和 slot-scope attribute 的 API 替代方案 -->
+      <!-- v-slot 只能在 template 或 组件标签上使用 -->
+
+      <!-- 将废除 slot="slot-name"(template|普通元素上)的写法 -->
+      <!-- <template v-slot:userSlot> -->
+
+      <template #userSlot="{ childSlotMsg }">
+        <h3 class="slot-header">
+          Here might be a Component Abc => {{ childSlotMsg }}
+        </h3>
+      </template>
+      <!-- <h3 slot="userSlot" class="slot-header">Here might be a Component Abc</h3> -->
+      <!-- </template> -->
+      <Abc />
+    </HelloWorld>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Abc from "./components/Abc.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Abc,
+  },
+  mounted() {
+    console.log("App.vue mounted => ", this);
+  },
+  methods: {
+    userClick(value, ev) {
+      console.log("自定义事件 => ", value, ev);
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Abc.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="abc-class">abc-component</div>
+</template>
+
+<script>
+export default {
+  name: "abc",
+};
+</script>
